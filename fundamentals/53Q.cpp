c02f2c939af47ac5f675bc52993e27d2ec5737e1
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2025-11-13 00:32:25 +0530

    today commit

--- fundamentals/53Q.cpp
+++ fundamentals/53Q.cpp
@@ -1,1 +1,37 @@
 //armstrong number
+
+#include <iostream>
+#include <cmath> // for pow() function
+using namespace std;
+
+int main() {
+    int num, originalNum, remainder, n = 0;
+    double result = 0.0;
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    originalNum = num;
+
+    // Count number of digits
+    while (originalNum != 0) {
+        originalNum /= 10;
+        ++n;
+    }
+
+    originalNum = num;
+
+    // Calculate sum of powers of digits
+    while (originalNum != 0) {
+        remainder = originalNum % 10;
+        result += pow(remainder, n);
+        originalNum /= 10;
+    }
+
+    if ((int)result == num)
+        cout << num << " is an Armstrong number." << endl;
+    else
+        cout << num << " is not an Armstrong number." << endl;
+
+    return 0;
+}
